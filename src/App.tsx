71880b71c439da40336952f5155991a258ff6fa6
Mhttt <74661052+Mhttt@users.noreply.github.com>
2022-11-26 22:56:50 +0100

    working filter on cities

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
 import './App.css';
 import Map, { Source, Layer, CircleLayer } from 'react-map-gl';
 import Filter from './components/Filter';
-import sportshaller from './geojson/sportshaller.json';
+import trashcansData from './geojson/affaldskurve.json';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { useState } from 'react';
+import TrashCans from './types';
+
+const trashcans = trashcansData as TrashCans
 
 const layerStyle: CircleLayer = {
 	id: 'point',
@@ -13,21 +17,36 @@ const layerStyle: CircleLayer = {
 	},
 };
 
+
 function App() {
+	const [viewState, setViewState] = useState({
+		longitude: 12.56,
+		latitude: 55.68,
+		zoom: 10,
+	});
+
+  const [selectedCity, setSelectedCity] = useState("")
+
+  const cities = trashcans.features.map(item => {
+    return item.properties.driftsbydel
+  })
+
+  const uniqueCities = Array.from(new Set(cities)).filter(item => item !== null)
+
+  const filteredCities = selectedCity !== "" ? trashcans.features.filter(feature => feature.properties.driftsbydel === selectedCity) : trashcans.features
+  const finalCities = {type: 'FeatureCollection', features: filteredCities}
+
 	return (
 		<div>
-			<Filter></Filter>
+			<Filter setSelectedCity={setSelectedCity} cities={uniqueCities}></Filter>
 			<Map
-				mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} //sæt ind i en env
-				initialViewState={{
-					longitude: 12.57,
-					latitude: 55.68,
-					zoom: 10,
-				}}
+				mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} 
+				{...viewState}
+				onMove={(evt) => setViewState(evt.viewState)}
 				style={{ width: '100vw', height: '100vh' }}
-				mapStyle="mapbox://styles/mapbox/streets-v9"
+				mapStyle="mapbox://styles/mapbox/streets-v11"
 			>
-				<Source id="my-data" type="geojson" data={sportshaller as any}>
+				<Source id="my-data" type="geojson" data={finalCities as any}>
 					<Layer {...layerStyle} />
 				</Source>
 			</Map>
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,21 @@
-function Filter() {
-	return <div>test</div>;
+import { FC } from "react";
+
+
+interface Props {
+  setSelectedCity: (value: string) => void,
+  cities: string[]
+}
+
+const Filter: FC<Props> = ({setSelectedCity, cities}) => {
+
+	return (
+		<select defaultValue={""} name="cities" onChange={(e) => setSelectedCity(e.target.value)}>
+			{cities.map((item) => (
+				<option key={item} value={item}>{item}</option>
+			))}
+			<option value={""}> All cities </option>
+		</select>
+	);
 }
 
 export default Filter;
