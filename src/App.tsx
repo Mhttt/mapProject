d72c1b7497a80ceb8d296b6988d85d8c2c5cca58
Mhttt <74661052+Mhttt@users.noreply.github.com>
2022-11-26 11:08:52 +0100

    Initial map setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Map, { Source, Layer, CircleLayer } from 'react-map-gl';
+import Filter from './components/Filter';
+import sportshaller from './geojson/sportshaller.json';
+
+const layerStyle: CircleLayer = {
+	id: 'point',
+	type: 'circle',
+	paint: {
+		'circle-radius': 5,
+		'circle-color': 'red',
+	},
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Filter></Filter>
+			<Map
+				mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} //sæt ind i en env
+				initialViewState={{
+					longitude: 12.57,
+					latitude: 55.68,
+					zoom: 10,
+				}}
+				style={{ width: '100vw', height: '100vh' }}
+				mapStyle="mapbox://styles/mapbox/streets-v9"
+			>
+				<Source id="my-data" type="geojson" data={sportshaller as any}>
+					<Layer {...layerStyle} />
+				</Source>
+			</Map>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Filter.tsx
@@ -0,0 +1,5 @@
+function Filter() {
+	return <div>test</div>;
+}
+
+export default Filter;
