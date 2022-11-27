efb247acbb1dee38a4b8a35006b8eb5cbf8cbedb
Mhttt <74661052+Mhttt@users.noreply.github.com>
2022-11-27 20:45:34 +0100

    working filter and nightmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import './App.css';
-import Map, { Source, Layer, CircleLayer } from 'react-map-gl';
+import Map, {
+	Source,
+	Layer,
+	CircleLayer,
+	NavigationControl,
+} from 'react-map-gl';
 import Filter from './components/Filter';
 import trashcansData from './geojson/affaldskurve.json';
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -7,16 +12,13 @@ import { useState } from 'react';
 import TrashCans from './types';
 import { Box } from '@mui/material';
 
-const trashcans = trashcansData as TrashCans
+const trashcans = trashcansData as TrashCans;
 
 const styles = {
 	container: {
 		position: 'relative',
 		width: '100vw',
 		height: '100vh',
-	},
-	search: {
-		position: 'sticky',
 	},
 };
 
@@ -29,41 +31,49 @@ const layerStyle: CircleLayer = {
 	},
 };
 
-
 function App() {
-
 	const [viewState, setViewState] = useState({
 		longitude: 12.56,
 		latitude: 55.68,
 		zoom: 10.5,
 	});
+	const [selectedCity, setSelectedCity] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
 
-  const [selectedCity, setSelectedCity] = useState("")
+	const cities = trashcans.features.map((item) => {
+		return item.properties.driftsbydel;
+	});
 
-  const cities = trashcans.features.map(item => {
-    return item.properties.driftsbydel
-  })
+	const uniqueCities = Array.from(new Set(cities)).filter(
+		(item) => item !== null
+	);
+	const filteredCities =
+		selectedCity !== ''
+			? trashcans.features.filter(
+					(feature) => feature.properties.driftsbydel === selectedCity
+			  )
+			: trashcans.features;
+	const finalCities = { type: 'FeatureCollection', features: filteredCities };
 
-  const uniqueCities = Array.from(new Set(cities)).filter(item => item !== null)
-  const filteredCities = selectedCity !== "" ? trashcans.features.filter(feature => feature.properties.driftsbydel === selectedCity) : trashcans.features
-  const finalCities = {type: 'FeatureCollection', features: filteredCities}
-
- 
+  console.log(darkMode);
 	return (
 		<Box sx={styles.container}>
 			<Map
 				mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
 				{...viewState}
 				onMove={(evt) => setViewState(evt.viewState)}
-				mapStyle="mapbox://styles/mapbox/streets-v11"
+				mapStyle={darkMode === false ? 'mapbox://styles/mapbox/streets-v11':'mapbox://styles/mapbox/dark-v11'}
 			>
 				<Filter
 					setSelectedCity={setSelectedCity}
+          darkMode={darkMode === true ? true : false}
+					setDarkMode={setDarkMode}
 					cities={uniqueCities}
 				></Filter>
 				<Source id="my-data" type="geojson" data={finalCities as any}>
 					<Layer {...layerStyle} />
 				</Source>
+				<NavigationControl style={{backgroundColor: darkMode === false ? 'white' :'grey'}} position="bottom-left"></NavigationControl>
 			</Map>
 		</Box>
 	);
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,24 +1,45 @@
 import {
 	Autocomplete,
+	Box,
+	IconButton,
 	TextField,
 } from '@mui/material';
 import { FC } from 'react';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
 
 interface Props {
 	setSelectedCity: (value: string) => void;
+	setDarkMode: (value: boolean) => void;
+  darkMode: boolean,
 	cities: string[];
 }
 
 const styles = {
 	searchBar: {
-		width: '300px',
-    padding: '20px'
+		width: '70%',
+		padding: '20px',
+		'& .MuiInputBase-root': {
+			backgroundColor: '#f8f8f8',
+		},
+	},
+	container: {
+		display: 'flex',
+		width: '500px',
+		height: '55px',
+	},
+	button: {
+		width: '10%',
+		top: '20px',
+		backgroundColor: 'white',
+		borderRadius: '10%',
+		color: 'black',
 	},
 };
 
-const Filter: FC<Props> = ({ setSelectedCity, cities }) => {
+const Filter: FC<Props> = ({ setSelectedCity, setDarkMode, cities, darkMode }) => {
 	return (
-		<>
+    
+		<Box sx={styles.container}>
 			<Autocomplete
 				freeSolo
 				onChange={(e, value) => {
@@ -28,7 +49,10 @@ const Filter: FC<Props> = ({ setSelectedCity, cities }) => {
 				sx={styles.searchBar}
 				renderInput={(params) => <TextField {...params} label="Search" />}
 			/>
-		</>
+			<IconButton disableRipple={false} sx={styles.button} onClick={() => {setDarkMode(!darkMode)}}> 
+        <ModeNightIcon></ModeNightIcon>
+       </IconButton>
+		</Box>
 	);
 };
 
